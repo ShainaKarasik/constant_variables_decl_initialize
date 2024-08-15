664589825a60e952df8915a75a0391c8ba5aaf9d
skarasik <shainanihon@gmail.com>
2024-08-15 17:21:08 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main() {
+	int num1 = 15, num2 = 100;
+	
+	std::cout << "num1(before): " << num1 << "\n";
+	
+	num1 = 75;
+	std::cout << "num1 (after):  " << num1;
+
+	std::cout << std::endl << std::endl;
+
+
+
+
+
+
+
+	return 0;
+
+}
